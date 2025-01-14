42d8347c5803d4e6cd0d10ab12c05301c7595666
Felix Campeau <4000329@keranna.qc.ca>
2025-01-13 19:59:02 -0500

    Progress in propertysavecheckermodule

---
+++ modules/propertysavechecker.py
@@ -0,0 +1,38 @@
+import wpilib
+from ntcore import NetworkTableInstance
+from wpilib import DriverStation, Timer
+
+from properties import loop_delay
+from ultime.module import Module
+
+entry_name_check_time = "/CheckSaveLoop/time"
+
+class PropertySaveCheckerModule(Module):
+    def __init__(self):
+        super().__init__()
+        inst = NetworkTableInstance.getDefault()
+        self.entry_check_time = inst.getEntry(entry_name_check_time)
+        self.timer_check = Timer()
+        self.timer_check.start()
+
+    def robotPeriodic(self) -> None:
+        from ultime.autoproperty import mode, PropertyMode
+
+        if mode != PropertyMode.Local:
+            if DriverStation.isFMSAttached():
+                if self.timer_check.advanceIfElapsed(10.0):
+                    wpilib.reportWarning(
+                        f"FMS is connected, but PropertyMode is not Local: {mode}"
+                    )
+            elif DriverStation.isDSAttached():
+                self.timer_check.start()
+                current_time = wpilib.getTime()
+                self.entry_check_time.setDouble(current_time)
+                if self.timer_check.advanceIfElapsed(loop_delay):
+                    mirror_time = self.entry_check_mirror.getDouble(0.0)
+                    if current_time - mirror_time < 5.0:
+                        print("Save loop running")
+                    else:
+                        raise RuntimeError(
+                            f"Save loop is not running ({current_time=:.2f}, {mirror_time=:.2f})"
+                        )
