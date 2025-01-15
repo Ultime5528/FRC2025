52163b388c56976d2558194b920fe6cdac74b81e
HayderChammakhi <4101192@keranna.qc.ca>
2025-01-15 18:56:02 -0500

    Added dashboard as a module

---
+++ modules/dashboard.py
@@ -0,0 +1,46 @@
+import commands2
+import wpilib
+
+from modules.hardware import HardwareModule
+from ultime.module import Module
+from ultime.subsystem import Subsystem
+
+"""
+Organiser par sous-système
+"""
+
+
+class DashboardModule(Module):
+    def __init__(self, hardware: HardwareModule):
+        super().__init__()
+        putSubsystemOnDashboard(hardware.drivetrain)
+
+
+def putCommandOnDashboard(
+    sub_table: str, cmd: commands2.Command, name: str = None, suffix: str = " commands"
+) -> commands2.Command:
+    if not isinstance(sub_table, str):
+        raise ValueError(
+            f"sub_table should be a str: '{sub_table}' of type '{type(sub_table)}'"
+        )
+
+    if suffix:
+        sub_table += suffix
+
+    sub_table += "/"
+
+    if name is None:
+        name = cmd.getName()
+    else:
+        cmd.setName(name)
+
+    wpilib.SmartDashboard.putData(sub_table + name, cmd)
+
+    return cmd
+
+
+def putSubsystemOnDashboard(subsystem: Subsystem, name: str = None):
+    if name is None:
+        name = subsystem.getName()
+
+    wpilib.SmartDashboard.putData(name, subsystem)
