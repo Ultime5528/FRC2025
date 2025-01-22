ae99997d75eba79d5fe06368a73760cda62d290e
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-01-22 18:16:34 -0500

    Changed creation order

--- robot.py
+++ robot.py
@@ -20,15 +20,15 @@ class Robot(ModuleRobot):
         self.hardware = HardwareModule()
         self.autonomous = AutonomousModule()
         self.control = ControlModule(self.hardware)
-        self.property_save_checker = PropertySaveCheckerModule()
         self.dashboard = DashboardModule(self.hardware)
+        self.property_save_checker = PropertySaveCheckerModule()
         self.battery_sim = BatterySimModule(self.hardware)
 
         self.addModules(
             self.hardware,
             self.autonomous,
             self.control,
+            self.dashboard,
             self.property_save_checker,
             self.battery_sim,
-            self.dashboard,
         )
