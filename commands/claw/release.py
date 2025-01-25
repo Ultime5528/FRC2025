e050c309ec8b999453ae865ad118dc8eb9054d6e
Charles R. <4201224@keranna.qc.ca>
2025-01-25 11:30:40 -0500

    Claw : using lambdas and added execute

--- commands/claw/release.py
+++ commands/claw/release.py
@@ -6,32 +6,31 @@ from ultime.autoproperty import autoproperty, FloatProperty, asCallable
 
 
 class Drop(Command):
-
     speed_level_1_left = autoproperty(0.5)
     speed_level_1_right = autoproperty(0.0)
     speed_level_2_left = autoproperty(0.5)
-    speed_level_2_right = autoproperty(0.5)
+    speed_level_2_right = autoproperty(-0.5)
     speed_level_3_left = autoproperty(0.5)
-    speed_level_3_right = autoproperty(0.5)
+    speed_level_3_right = autoproperty(-0.5)
     speed_level_4_left = autoproperty(0.5)
-    speed_level_4_right = autoproperty(0.5)
+    speed_level_4_right = autoproperty(-0.5)
     delay = autoproperty(1.0)
 
     @classmethod
     def atLevel1(cls, claw: Claw):
-        return cls(claw, Drop.speed_level_1_left, Drop.speed_level_1_right)
+        return cls(claw, lambda: cls.speed_level_1_left, lambda: cls.speed_level_1_right)
 
     @classmethod
     def atLevel2(cls, claw: Claw):
-        return cls(claw, Drop.speed_level_2_left, Drop.speed_level_2_right)
+        return cls(claw, lambda: cls.speed_level_2_left, lambda: cls.speed_level_2_right)
 
     @classmethod
     def atLevel3(cls, claw: Claw):
-        return cls(claw, Drop.speed_level_3_left, Drop.speed_level_3_right)
+        return cls(claw, lambda: cls.speed_level_3_left, lambda: cls.speed_level_3_right)
 
     @classmethod
     def atLevel4(cls, claw: Claw):
-        return cls(claw, Drop.speed_level_4_left, Drop.speed_level_4_right)
+        return cls(claw, lambda: cls.speed_level_4_left, lambda: cls.speed_level_4_right)
 
     def __init__(
         self,
@@ -46,13 +45,14 @@ class Drop(Command):
         self.timer = wpilib.Timer()
 
     def initialize(self):
+        self.timer.reset()
+        self.timer.start()
+
+    def execute(self):
         speed_left = self.get_speed_left()
         self.claw.setLeft(speed_left)
         speed_right = self.get_speed_right()
         self.claw.setRight(speed_right)
-
-        self.timer.reset()
-        self.timer.start()
 
     def isFinished(self) -> bool:
         return self.timer.hasElapsed(self.delay)
