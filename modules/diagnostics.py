1a19f4a9e31af1f20a73299b9239b0fc6bf13670
LateAlways <javiparedes08@outlook.com>
2025-01-25 13:48:06 -0500

    Ran black.

--- modules/diagnostics.py
+++ modules/diagnostics.py
@@ -11,9 +11,7 @@ from ultime.module import Module, ModuleList
 class DiagnosticsModule(Module):
     def __init__(self, hardware: HardwareModule, module_list: ModuleList):
         super().__init__()
-        self.components_tests: List[commands2.Command] = [
-
-        ]
+        self.components_tests: List[commands2.Command] = []
 
         self._hardware = hardware
         self._module_list = module_list
