8dc792bda6999867ece5c9811c5719f2e394568e
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-01-29 17:54:25 -0500

    Changed modules order

--- robot.py
+++ robot.py
@@ -21,19 +21,19 @@ class Robot(ModuleRobot):
         self.enableLiveWindowInTest(True)
 
         self.hardware = HardwareModule()
-        self.dashboard = DashboardModule(self.hardware, self.modules)
-        self.autonomous = AutonomousModule()
         self.control = ControlModule(self.hardware)
-        self.property_save_checker = PropertySaveCheckerModule()
+        self.autonomous = AutonomousModule()
+        self.dashboard = DashboardModule(self.hardware, self.modules)
         self.diagnostics = DiagnosticsModule(self.hardware, self.modules)
+        self.property_save_checker = PropertySaveCheckerModule()
         self.battery_sim = BatterySimModule(self.hardware)
 
         self.addModules(
             self.hardware,
-            self.dashboard,
-            self.autonomous,
-            self.diagnostics,
             self.control,
+            self.autonomous,
+            self.dashboard,
+            self.diagnostics,
             self.property_save_checker,
             self.battery_sim,
         )
