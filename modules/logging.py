637ec1104768455971523024498d9a03c9f7d76d
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-02-03 18:41:41 -0500

    Enable logging but URCL in CI

--- modules/logging.py
+++ modules/logging.py
@@ -1,6 +1,5 @@
 import os
 
-# from urcl import URCL
 from wpilib import DataLogManager, DriverStation
 
 import ports
@@ -11,19 +10,21 @@ class LoggingModule(Module):
     def __init__(self):
         super().__init__()
 
-        # can_id_aliases = dict()
-        #
-        # for attr, value in ports.CAN.__dict__.items():
-        #     if not attr.startswith("__"):
-        #         assert isinstance(value, int)
-        #         can_id_aliases[value] = attr
-        #
-        # print("Logging with CAN ID aliases:", can_id_aliases)
-        #
-        # if os.environ.get("GITHUB_ACTIONS"):
-        #     print("Testing in CI: Logging is disabled")
-        # else:
-        #     print("Not in CI: Logging is enabled")
-        #     URCL.start(can_id_aliases)
-        #     DataLogManager.start()
-        #     DriverStation.startDataLog(DataLogManager.getLog())
+        can_id_aliases = dict()
+
+        for attr, value in ports.CAN.__dict__.items():
+            if not attr.startswith("__"):
+                assert isinstance(value, int)
+                can_id_aliases[value] = attr
+
+        print("Logging with CAN ID aliases:", can_id_aliases)
+
+        if os.environ.get("CI"):
+            print("Testing in CI: URCL is disabled")
+        else:
+            print("Not in CI: URCL is enabled")
+            import urcl
+            urcl.URCL.start(can_id_aliases)
+
+        DataLogManager.start()
+        DriverStation.startDataLog(DataLogManager.getLog())
