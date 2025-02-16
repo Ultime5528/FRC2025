f3bd32f521b2e8732c87f1679d1564c44cc15f09
Vincent <4100046@keranna.qc.ca>
2025-02-16 14:53:55 -0500

    Finished resetAll command

--- commands/resetall.py
+++ commands/resetall.py
@@ -1,28 +1,37 @@
-from commands2 import SequentialCommandGroup, ParallelCommandGroup
+from commands2 import SequentialCommandGroup
+from commands2.cmd import parallel
 
 from commands.arm.retractarm import RetractArm
-from commands.elevator.moveelevator import MoveElevator
+from commands.climber.resetclimber import ResetClimber
+from commands.elevator.manualmoveelevator import ManualMoveElevator
 from commands.elevator.resetelevator import ResetElevator
 from commands.intake.resetintake import ResetIntake
 from commands.printer.resetright import ResetPrinterRight
 from subsystems.arm import Arm
+from subsystems.climber import Climber
 from subsystems.elevator import Elevator
 from subsystems.intake import Intake
 from subsystems.printer import Printer
+from ultime.command import ignore_requirements
 
 
+@ignore_requirements(["elevator", "printer", "arm", "intake", "climber"])
 class ResetAll(SequentialCommandGroup):
-    def __init__(self, elevator: Elevator, printer: Printer, arm: Arm, intake: Intake):
+    def __init__(
+        self,
+        elevator: Elevator,
+        printer: Printer,
+        arm: Arm,
+        intake: Intake,
+        climber: Climber,
+    ):
         super().__init__(
-            ParallelCommandGroup(
+            ManualMoveElevator.up(elevator).withTimeout(0.5),
+            RetractArm(arm),
+            parallel(
                 ResetElevator(elevator),
                 ResetPrinterRight(printer),
-            ),
-            MoveElevator.toLevel1(elevator),
-            ParallelCommandGroup(
-                RetractArm(arm),
-                # ResetClimber,
                 ResetIntake(intake),
+                ResetClimber(climber),
             ),
-            MoveElevator.toLoading(elevator),
         )
