b1551770fe7b753ad3f1143ba417cc8d2ae31415
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-02-17 18:43:16 -0500

    Black + optimized imports

--- robot.py
+++ robot.py
@@ -5,12 +5,12 @@ from modules.armcollision import ArmCollision
 from modules.autonomous import AutonomousModule
 from modules.batterysim import BatterySimModule
 from modules.control import ControlModule
+from modules.coralretraction import CoralRetractionModule
 from modules.dashboard import DashboardModule
 from modules.diagnostics import DiagnosticsModule
 from modules.hardware import HardwareModule
 from modules.logging import LoggingModule
 from modules.propertysavechecker import PropertySaveCheckerModule
-from modules.coralretraction import CoralRetractionModule
 from modules.vision import VisionModule
 from ultime.modulerobot import ModuleRobot
 
@@ -34,7 +34,9 @@ class Robot(ModuleRobot):
         self.battery_sim = BatterySimModule(self.hardware)
         self.arm_collision = ArmCollision(self.hardware)
         self.vision = VisionModule()
-        self.coral_retraction = CoralRetractionModule(self.hardware.elevator, self.hardware.claw)
+        self.coral_retraction = CoralRetractionModule(
+            self.hardware.elevator, self.hardware.claw
+        )
 
         self.addModules(
             self.hardware,
