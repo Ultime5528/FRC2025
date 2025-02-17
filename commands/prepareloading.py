ffebb249b288c3941b353d73ed491f8045d7ac80
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-02-16 20:15:08 -0500

    Renamed commang

--- commands/prepareloading.py
+++ commands/prepareloading.py
@@ -6,9 +6,11 @@ from commands.printer.moveprinter import MovePrinter
 from subsystems.arm import Arm
 from subsystems.elevator import Elevator
 from subsystems.printer import Printer
+from ultime.command import ignore_requirements
 
 
-class PrepareLoadingPosition(ParallelCommandGroup):
+@ignore_requirements(["elevator", "arm", "printer"])
+class PrepareLoading(ParallelCommandGroup):
     def __init__(self, elevator: Elevator, arm: Arm, printer: Printer):
         super().__init__(
             MoveElevator.toLoading(elevator),
