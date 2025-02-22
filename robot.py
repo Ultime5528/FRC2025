b738a609904dfd5045e7068110a8a251f53dad47
Vincent <4100046@keranna.qc.ca>
2025-02-22 14:04:23 -0500

    Fixed vision in robot

--- robot.py
+++ robot.py
@@ -33,7 +33,6 @@ class Robot(ModuleRobot):
         self.property_save_checker = PropertySaveCheckerModule()
         self.battery_sim = BatterySimModule(self.hardware)
         self.arm_collision = ArmCollision(self.hardware)
-        # self.vision = VisionModule()
         self.coral_retraction = CoralRetractionModule(
             self.hardware.elevator, self.hardware.claw
         )
@@ -47,7 +46,7 @@ class Robot(ModuleRobot):
             self.diagnostics,
             self.logging,
             self.property_save_checker,
-            # self.vision,
+            self.vision,
             self.arm_collision,
             self.coral_retraction,
             # self.battery_sim,  # Current becomes so low, robot stops working
