1682a9e99a9c88d924edd29489660404d7cbbe7f
ThomasMcDonald <thomas.mcdonald866@gmail.com>
2025-02-22 11:42:35 -0500

    WIP

--- modules/control.py
+++ modules/control.py
@@ -7,4 +7,34 @@ class ControlModule(Module):
         super().__init__()
         self.hardware = hardware
 
-        # self.hardware.controller.button(1).onTrue(Command())
+        self.setupButtons()
+
+    def setupButtons(self):
+        def setupButtons(self):
+            """
+            Bind commands to buttons on controllers and joysticks
+            """
+            # Example code for xbox_controller
+            # self.hardware.controller.leftTrigger().whileTrue(
+            #     AlignedPickUp(self.drivetrain, self.intake, self.vision_pick_up)
+            # )
+
+            # Copilot's panel
+            AxisTrigger(self.panel_1, 1, "down").whileTrue(ExtendClimber(self.climber_left))
+            AxisTrigger(self.panel_1, 1, "up").whileTrue(RetractClimber(self.climber_left))
+            self.panel_1.button(3).onTrue(PickUp(self.intake))
+            self.panel_1.button(2).onTrue(Drop(self.intake))
+            self.panel_1.button(1).onTrue(MovePivot.toSpeakerClose(self.pivot))
+
+            AxisTrigger(self.panel_2, 1, "down").whileTrue(
+                ExtendClimber(self.climber_right)
+            )
+            AxisTrigger(self.panel_2, 1, "up").whileTrue(RetractClimber(self.climber_right))
+            self.panel_2.button(2).onTrue(
+                PrepareAndShootAndMovePivotLoading(self.shooter, self.pivot, self.intake)
+            )
+            self.panel_2.button(5).onTrue(
+                ShootAndMovePivotLoading(self.shooter, self.intake, self.pivot)
+            )
+            self.panel_2.button(1).onTrue(MovePivot.toAmp(self.pivot))
+            self.panel_2.button(4).onTrue(ResetPivotDown(self.pivot))
--- modules/hardware.py
+++ modules/hardware.py
@@ -17,6 +17,8 @@ class HardwareModule(Module):
     def __init__(self):
         super().__init__()
         self.controller = commands2.button.CommandXboxController(0)
+        self.panel_1 = commands2.button.CommandJoystick(1)
+        self.panel_2 = commands2.button.CommandJoystick(2)
 
         self.drivetrain = Drivetrain()
         self.drivetrain.setDefaultCommand(DriveField(self.drivetrain, self.controller))
