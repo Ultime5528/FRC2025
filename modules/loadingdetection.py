46731054d8b517da155e478984900c2492b54d4f
Charles R. <4201224@keranna.qc.ca>
2025-02-24 17:43:01 -0500

    Schedule load command in loading detection module

--- modules/loadingdetection.py
+++ modules/loadingdetection.py
@@ -1,3 +1,4 @@
+from commands.claw.loadcoral import LoadCoral
 from modules.hardware import HardwareModule
 from ultime.module import Module
 
@@ -8,9 +9,18 @@ class LoadingDetection(Module):
         self.claw = hardware.claw
         self.elevator = hardware.elevator
         self.printer = hardware.printer
+        self._load_command = LoadCoral(self.claw)
 
     def robotPeriodic(self) -> None:
         self.claw.is_at_loading = (
             self.printer.state == self.printer.State.Loading
             and self.elevator.state == self.elevator.State.Loading
         )
+
+        if (
+            self.claw.seesObject()
+            and not self.claw.has_coral
+            and self.printer.state == self.printer.State.Loading
+            and self.elevator.state == self.elevator.State.Loading
+        ):
+            self._load_command.schedule()
