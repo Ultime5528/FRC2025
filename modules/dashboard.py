585a321ad37605ab3e53ca7df3370959eb6f5565
Felix Campeau <campeaufelix@gmail.com>
2025-02-24 17:26:35 -0500

    resetallbutclimber done not tested

--- modules/dashboard.py
+++ modules/dashboard.py
@@ -23,6 +23,7 @@ from commands.printer.moveprinter import MovePrinter
 from commands.printer.resetprinter import ResetPrinterRight
 from commands.printer.scanprinter import ScanPrinter
 from commands.resetall import ResetAll
+from commands.resetallbutclimber import ResetAllButClimber
 from modules.hardware import HardwareModule
 from ultime.module import Module, ModuleList
 
@@ -117,6 +118,15 @@ class DashboardModule(Module):
                 hardware.intake,
                 hardware.climber,
             ),
+        )
+        putCommandOnDashboard(
+            "Group",
+            ResetAllButClimber(
+                hardware.elevator,
+                hardware.printer,
+                hardware.arm,
+                hardware.intake,
+            )
         )
         putCommandOnDashboard(
             "Group", PrepareLoading(hardware.elevator, hardware.arm, hardware.printer)
