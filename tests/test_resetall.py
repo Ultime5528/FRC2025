bf54c974d52fa45d40bd222b0d7cb573eba16c35
Felix Campeau <4000329@keranna.qc.ca>
2025-02-24 20:01:47 -0500

    very perplexing wip

--- tests/test_resetall.py
+++ tests/test_resetall.py
@@ -1,8 +1,9 @@
+import pytest
+
 from commands.resetall import ResetAll
 from commands.resetallbutclimber import ResetAllButClimber
 from robot import Robot
 from ultime.tests import RobotTestController
-
 
 def test_reset_all(robot_controller: RobotTestController, robot: Robot):
     robot_controller.startTeleop()
@@ -26,6 +27,7 @@ def test_reset_all(robot_controller: RobotTestController, robot: Robot):
     assert climber._has_reset
 
 
+@pytest.mark.felix
 def test_reset_all_but_climber(robot_controller: RobotTestController, robot: Robot):
     robot_controller.startTeleop()
     elevator = robot.hardware.elevator
@@ -36,9 +38,11 @@ def test_reset_all_but_climber(robot_controller: RobotTestController, robot: Rob
     cmd = ResetAllButClimber(elevator, printer, arm, intake)
     cmd.schedule()
 
+    robot_controller.wait(1.0)
+    elevator._switch.setSimPressed()
     robot_controller.wait_until(lambda: not cmd.isScheduled(), 10.0)
 
-    assert elevator.hasReset()
     assert printer.hasReset()
     assert arm.state == arm.State.Retracted
     assert intake.hasReset()
+    assert elevator.hasReset()
