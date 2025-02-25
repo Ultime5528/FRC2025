1ddbc0afcf619b6dd4b3aa8cf121c0e71faa6dd8
Vincent <4100046@keranna.qc.ca>
2025-02-24 19:23:09 -0500

    Made align with algae

--- modules/control.py
+++ modules/control.py
@@ -1,10 +1,16 @@
+import robot
+from commands.vision.alignwithalgae import AlignWithAlgae
+from modules.algaevision import AlgaeVisionModule
 from modules.hardware import HardwareModule
+from robot import Robot
 from ultime.module import Module
 
 
 class ControlModule(Module):
-    def __init__(self, hardware: HardwareModule):
+    def __init__(self, hardware: HardwareModule, vision_algae: AlgaeVisionModule):
         super().__init__()
         self.hardware = hardware
+        self.algae_vision = vision_algae
 
+        self.hardware.controller.rightTrigger().whileTrue(AlignWithAlgae(hardware.drivetrain, self.algae_vision, hardware.controller))
         # self.hardware.controller.button(1).onTrue(Command())
