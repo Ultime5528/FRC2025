7bf6c3486ea972f96eb8b66decc5565f238bd9fb
Charles R. <4201224@keranna.qc.ca>
2025-03-01 15:02:03 -0500

    Ready To Test MegaCommandOldSchool

--- commands/autonomous/megacommandcode.py
+++ commands/autonomous/megacommandcode.py
@@ -1,7 +1,12 @@
 from commands2 import SequentialCommandGroup, ParallelCommandGroup
-from commands2.cmd import parallel
+from commands2.cmd import parallel, sequence
 
+from commands.arm.extendarm import ExtendArm
+from commands.claw.waituntilcoral import WaitUntilCoral
+from commands.climber.resetclimber import ResetClimber
 from commands.drivetrain.drivetoposes import DriveToPoses, pose
+from commands.dropprepareloading import DropPrepareLoading
+from commands.elevator.moveelevator import MoveElevator
 from commands.resetallbutclimber import ResetAllButClimber
 from subsystems.arm import Arm
 from subsystems.claw import Claw
@@ -24,14 +29,46 @@ class MegaCommandCode(SequentialCommandGroup):
     ):
         super().__init__(
             parallel(
-            ResetAllButClimber(elevator, printer, arm, intake),
-            DriveToPoses.fromRedBluePoints(
-                drivetrain,
-                [
-                    pose()
-                ],
-                [
-                    pose(5.1359, 2.9006)
-                ]
-                )
+                DriveToPoses.fromRedBluePoints(
+                    drivetrain,
+                    [
+                        pose(12.4127, 5.1311, -60.0)
+                    ],
+                    [
+                        pose(5.1359, 2.9006, 120.0)
+                    ]
+                ),
+                sequence(
+                    ResetAllButClimber(elevator, printer, arm, intake),
+                    MoveElevator.toLevel2(elevator),
+                ),
             ),
+            DropPrepareLoading.toRight(),
+            parallel(
+                DriveToPoses.fromRedBluePoints(
+                    drivetrain,
+                    [
+                        pose(16.0406, 7.23474, 146.0)
+                    ],
+                    [
+                        pose(1.508, 0.797, -34.0)
+                    ],
+                ),
+                ResetClimber(),
+            ),
+            WaitUntilCoral(claw),
+            parallel(
+                DriveToPoses.fromRedBluePoints(
+                    drivetrain,
+                    [
+                        pose(13.7059, 5.1310, -120.0)
+                    ],
+                    [
+                        pose(3.8427, 2.9008, 60.0)
+                    ]
+                ),
+                MoveElevator.toLevel4(elevator),
+                ExtendArm(arm),
+            ),
+            DropPrepareLoading.toRight(),
+        )
