da1b1133cc4133ab50ff943e86b2d03af15db47f
Charles R. <4201224@keranna.qc.ca>
2025-03-02 10:18:26 -0500

    Removed dead code

--- modules/autonomous.py
+++ modules/autonomous.py
@@ -45,10 +45,6 @@ class AutonomousModule(Module):
         #     self.hardware.drivetrain,
         # )
 
-        # Flipping must be done by the command because the AutoBuilder uses custom code
-        self.createFollowPathCommand = lambda path: _FollowPathplannerPath(
-            path, self.hardware.drivetrain
-        )
 
         AutoBuilder.configureCustom(
             lambda path: FollowPath(path, self.hardware.drivetrain),
