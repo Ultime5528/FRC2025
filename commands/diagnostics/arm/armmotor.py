1e5bcb54770aaa8657db1d8e6180712b3cc98f52
LateAlways <javiparedes08@outlook.com>
2025-03-04 08:58:37 -0500

    Comments from 143

--- commands/diagnostics/arm/armmotor.py
+++ commands/diagnostics/arm/armmotor.py
@@ -6,7 +6,7 @@ from commands2 import (
     FunctionalCommand,
 )
 from commands2.cmd import runOnce, parallel, sequence
-from wpilib import RobotController
+from wpilib import RobotController, DataLogManager
 
 from commands.arm.extendarm import ExtendArm
 from commands.arm.retractarm import RetractArm
@@ -61,9 +61,14 @@ class DiagnoseArmMotor(SequentialCommandGroup):
         self.arm.stop()
         voltage_delta_before = self.voltage_before - self.voltage_during
         voltage_delta_after = self.voltage_after - self.voltage_during
-        print("Voltage delta before:", voltage_delta_before)
-        print("Voltage delta after:", voltage_delta_after)
-        if voltage_delta_before < self.voltage_change_threshold:
-            self.arm.alert_motor.set(True)
-        elif voltage_delta_after < self.voltage_change_threshold:
+        DataLogManager.log(
+            "Arm diagnostics: voltage delta before" + str(voltage_delta_before)
+        )
+        DataLogManager.log(
+            "Arm diagnostics: voltage delta after" + str(voltage_delta_after)
+        )
+        if (
+            voltage_delta_before < self.voltage_change_threshold
+            or voltage_delta_after < self.voltage_change_threshold
+        ):
             self.arm.alert_motor.set(True)
