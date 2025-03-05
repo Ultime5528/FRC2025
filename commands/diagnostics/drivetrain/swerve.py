549ba1d61b36898a6969b877fe6c8b8d337ab219
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-03-05 08:35:31 -0500

    Shorter equivalent swerve test

--- commands/diagnostics/drivetrain/swerve.py
+++ commands/diagnostics/drivetrain/swerve.py
@@ -1,6 +1,5 @@
-import wpilib
-from commands2 import SequentialCommandGroup, FunctionalCommand
-from commands2.cmd import runOnce, parallel
+from commands2 import SequentialCommandGroup
+from commands2.cmd import runOnce, run, waitSeconds, deadline
 from wpimath.geometry import Rotation2d
 from wpimath.kinematics import SwerveModuleState
 
@@ -19,49 +18,31 @@ class DiagnoseSwerveModule(SequentialCommandGroup):
         self, swerve: SwerveModule, alert_encoders: Alert, alert_turning_motor: Alert
     ):
         super().__init__(
-            runOnce(proxy(self.before_test)),
-            parallel(  # for driving motor
-                FunctionalCommand(
-                    lambda: None,
-                    lambda: self.swerve._driving_motor.set(0.2),
-                    lambda _: None,
-                    lambda: self.timer.get() > 1,
-                ),
-                FunctionalCommand(
-                    lambda: None,
-                    proxy(self.test_encoder),
-                    lambda _: None,
-                    lambda: self.timer.get() > 1,
-                ),
+            deadline(  # for driving motor
+                waitSeconds(1.0),
+                run(proxy(self.test_encoder)),
             ),
             runOnce(proxy(self.after_encoder_test)),
-            runOnce(proxy(self.before_test)),
-            FunctionalCommand(  # for turning motor
-                lambda: None,
-                lambda: self.swerve.setDesiredState(self.swerve_module_state_test),
-                lambda _: None,
-                lambda: self.timer.get() > 1,
+            deadline(  # for driving motor
+                waitSeconds(1.0),
+                run(lambda: self.swerve.setDesiredState(self.swerve_module_state_test)),
             ),
             runOnce(proxy(self.after_turning_motor_test)),
         )
         self.swerve_module_state_test = SwerveModuleState(
             0, Rotation2d.fromDegrees(self.target_angle)
         )
-        self.timer = wpilib.Timer()
         self.swerve = proxy(swerve)
         self.max_velocity = 0
         self.alert_encoders = alert_encoders
         self.alert_turning_motor = alert_turning_motor
 
-    def before_test(self):
-        self.timer.reset()
-        self.timer.start()
-
     def test_encoder(self):
-        if self.timer.get() > 0.1:
-            self.max_velocity = max(self.swerve.getVelocity(), self.max_velocity)
+        self.swerve._driving_motor.set(0.2)
+        self.max_velocity = max(self.swerve.getVelocity(), self.max_velocity)
 
     def after_encoder_test(self):
+        self.swerve._driving_motor.set(0.0)
         if self.max_velocity < self.min_velocity:
             self.alert_encoders.set(True)
 
