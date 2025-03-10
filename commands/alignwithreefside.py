86756d34e638f508e31629b1a0ed3fcbceb24479
ThomasMcDonald <thomas.mcdonald866@gmail.com>
2025-03-10 19:35:59 -0400

    Working code

--- commands/alignwithreefside.py
+++ commands/alignwithreefside.py
@@ -71,9 +71,7 @@ class AlignWithReefSide(DeferredCommand):
         self.addRequirements(drivetrain)
 
     def createCommand(self) -> Command:
-        return DriveToPoses(
-            self.drivetrain, [self.getTagPoseToAlign()], 10.5, 5.0, 15.0, 5.0
-        )
+        return DriveToPoses(self.drivetrain, [self.getTagPoseToAlign()])
 
     def getTagPoseToAlign(self) -> Pose2d:
         tag = getClosestReefTagID(self.drivetrain.getPose())
