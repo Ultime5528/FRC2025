3308bed1575a71c42a4badb10f730f88ae622135
Étienne Thibault <4001491@keranna.qc.ca>
2025-03-10 20:07:48 -0400

    Timeout for waituntilcoral

--- commands/claw/waituntilcoral.py
+++ commands/claw/waituntilcoral.py
@@ -1,6 +1,8 @@
+import wpilib
 from commands2 import Command
 
 from subsystems.claw import Claw
+from ultime.autoproperty import autoproperty
 from ultime.command import ignore_requirements
 
 
@@ -10,6 +12,18 @@ class WaitUntilCoral(Command):
     def __init__(self, claw: Claw):
         super().__init__()
         self.claw = claw
+        self.timer = wpilib.Timer()
+
+    def initialize(self):
+        self.timer.stop()
+        self.timer.reset()
+        self.timer.start()
 
     def isFinished(self) -> bool:
-        return self.claw.seesObject()
+        return self.claw.seesObject() or self.timer.get() >= _properties.timeout
+
+class _ClassProperties:
+    timeout = autoproperty(3, subtable=WaitUntilCoral.__name__)
+
+
+_properties=_ClassProperties()
