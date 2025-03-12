da5121bfb19cb0a91b19c2a1a7d0819f937a33a9
Thomas McDonald <53022931+ThomasMcDonald904@users.noreply.github.com>
2025-03-12 13:17:32 -0400

    Finished movehorizontal.py

--- commands/drivetrain/movehorizontal.py
+++ commands/drivetrain/movehorizontal.py
@@ -3,13 +3,21 @@ from ultime.command import Command
 
 class MoveHorizontal(Command):
     @classmethod
-    def moveRight(cls, drivetrain: Drivetrain):
+    def right(cls, drivetrain: Drivetrain):
         cmd = MoveHorizontal(drivetrain, -0.2)
+        cmd.setName(MoveHorizontal.__name__ + ".right")
         return cmd
 
+    @classmethod
+    def left(cls, drivetrain: Drivetrain):
+        cmd = MoveHorizontal(drivetrain, 0.2)
+        cmd.setName(MoveHorizontal.__name__ + ".left")
+        return cmd
+        
     def __init__(self, drivetrain: Drivetrain, speed: float):
         super().__init__()
         self.drivetrain = drivetrain
+        self.addRequirements(drivetrain)
         self.speed = speed
 
     def execute(self):
