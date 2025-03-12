68824d5eb8c8c49000f4262c5a9020efebf51b19
Charles R. <4201224@keranna.qc.ca>
2025-03-12 18:15:04 -0400

    Removed two pathplanner commands and finished auto level4 tests

--- tests/test_autonomouslevel4.py
+++ tests/test_autonomouslevel4.py
@@ -1,15 +1,14 @@
-import pytest
 import os
 
 
-@pytest.mark.specific
-def test_auonome_level_4():
+def test_auto_level_4():
 
     path = "deploy/pathplanner/autos"
     for filename in os.listdir(path):
         if filename.endswith(".auto"):
             with open(os.path.join(path, filename)) as file:
-                assert not '.toLevel1' in file.read()
-                assert not '.toLevel2' in file.read()
-                assert not '.toLevel3' in file.read()
-                assert '.toLevel4' in file.read()
+                content = file.read()
+                assert not "MoveElevator.toLevel1" in content, f"{str(filename)}"
+                assert not "MoveElevator.toLevel2" in content, f"{str(filename)}"
+                assert not "MoveElevator.toLevel3" in content, f"{str(filename)}"
+                assert "MoveElevator.toLevel4" in content, f"{str(filename)}"
