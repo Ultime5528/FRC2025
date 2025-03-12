447495bb53fd14a8171d40e9126fc971e273fa46
Étienne Beaulac <etienne.beaulac@outlook.com>
2025-03-12 18:39:47 -0400

    Moved property inside class

--- commands/claw/waituntilcoral.py
+++ commands/claw/waituntilcoral.py
@@ -8,6 +8,7 @@ from ultime.command import ignore_requirements
 
 @ignore_requirements(["claw"])
 class WaitUntilCoral(Command):
+    timeout = autoproperty(3.0)
 
     def __init__(self, claw: Claw):
         super().__init__()
@@ -15,16 +16,7 @@ class WaitUntilCoral(Command):
         self.timer = wpilib.Timer()
 
     def initialize(self):
-        self.timer.stop()
-        self.timer.reset()
-        self.timer.start()
+        self.timer.restart()
 
     def isFinished(self) -> bool:
-        return self.claw.seesObject() or self.timer.get() >= _properties.timeout
-
-
-class _ClassProperties:
-    timeout = autoproperty(3, subtable=WaitUntilCoral.__name__)
-
-
-_properties = _ClassProperties()
+        return self.claw.seesObject() or self.timer.hasElapsed(self.timeout)
